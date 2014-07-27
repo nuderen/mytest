478673dba61e9b5ca8c9fe8f7c42247430009e04
kazu_vagrant_gm <dignity_of_soldier_light@yahoo.co.jp>
2014-07-27 03:30:56 +0000

    add file

--- httpdversion.rb
+++ httpdversion.rb
@@ -1,7 +1,9 @@
+#HTTPversion --presentation of HTTP version
 class HTTPVersion
 	attr_accessor :major
 	attr_accessor :minor
 
+  #convert version into a HTTPversion
 	def self.convert(version)
 		version.is_a?(self) ? version : new(version)
 	end
@@ -13,19 +15,20 @@ def initialize(version)
   when String
     if /^(\d+)\.(\d+)$/ =~ version
       @major, @minor = $1.to_i, $2.to_i
-    end 
-  end 
+    end
+  end
   if @major.nil? || @minor.nil?
     raise ArgumentError,
       format("cannot convert %s into %s", version.class, self.class)
-  end 
-end 
+  end
+end
 end
 
 h1 = HTTPVersion.new('1.1')
 h2 = HTTPVersion.new('1.2')
 #h3 = HTTPVersion.new('331.5')
-h3 = HTTPVersion.new('5d')
+# h3 = HTTPVersion.new('5d')
 p h1
 p h2
-p h3
+
+h1.convert("1,2")
--- job.rb
+++ job.rb
@@ -2,16 +2,13 @@ require 'net/http'
 require 'uri'
 =begin
 p Net::HTTP.version_1_2
-
 Net::HTTP.start("https://www.ruby-lang.org/",80){|http|
 	response = http.get('/ja/')
 	puts response.body
 }
 //////
 Net::HTTP.get_print 'www.ruby-lang.org','/index.html'
-
 Net::HTTP.get_print URI.parse('https://www.ruby-lang.org/ja/')
-
 #url = URI.parse('http://www.example.com/index.html')
 #url = URI.parse('https://www.ruby-lang.org/ja/') #httpsはだめ
 url = URI.parse('http://news.yahoo.co.jp/pickup/6124544')
@@ -20,10 +17,50 @@ res = Net::HTTP.start(url.host, url.port) {|http|
   http.request(req)
 }
 puts res.body
-
 =end
 
 puts Net::HTTP.version_1_1?
 puts Net::HTTP.version_1_2?
+arr = [1,2,3]
+p arr.is_a?(Hash)
+p arr.is_a?(Array)
 
-puts HTTPVersion
+# selt << の定義##########################################
+# http://magazine.rubyist.net/?0046-SingletonClassForBeginners
+# defiune peculiar method to object -> define one's own method direcotry
+hello = 'hello'
+def hello.say(count=1)
+	count.times{print self}
+end
+hello.say 2
+
+# bad exmaple
+another_hello = "hello"
+#another_hello.say 3
+
+# class << object => pull peculiar class of object and defiune peculiar method to object
+# definition of a peculiar obeject!
+class << hello
+	def say_world
+		puts "#{self}, world!"
+	end
+end
+hello.say_world
+
+class Foo
+	class << self
+
+
+
+########################################
+
+
+
+
+
+
+
+
+
+
+
